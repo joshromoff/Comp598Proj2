83bba464f8f5384173a911f693e5962c7f4ffad4
joshromoff <joshua.romoff@mail.mcgill.ca>
2015-10-09 01:14:39 -0400

    Optimized parsing by using sklearn vectorization, currently uses all words that arent stop words and that appear in at least 2 of the transcripts.

--- parseTokenize.py
+++ parseTokenize.py
@@ -2,58 +2,70 @@ import nltk
 import csv
 from random import randint
 import pickle
-
-
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
 #the goal here is to only have to parse the data once
 #save it as dictionarys to be accessed by every classifier we test.
 
 testCSV  = "./DataSetCSVs/ml_dataset_test_in.csv"
 trainCSV = "./DataSetCSVs/ml_dataset_train.csv"
 
-trainingSet     = {}
-validationSet  = {}
-        
+
+
 #open training csv and parse the data
 #format {index,text,class}
 f        = open(trainCSV,'rb')
 training = csv.reader(f)
 
-counter = 0
+trainingCorpus = []
+trainingY      = []
+
+#loop through training set, accumulated the text 
 for row in training:
-    if (row[0]).isdigit():
-        text       = nltk.word_tokenize(row[1].decode('utf-8'))
-        index      = int(row[0])
-        classifier = int(row[2])
-        if index % 1000 ==0:
-            print index ,' of training'
+    if row[0].isdigit():
+        trainingCorpus.append(row[1])
+        trainingY.append(int(row[2]))
 
-       
-        trainingSet[index] = (text,classifier)
+f.close()
 
-    
-    
-#open test csv and parse/tockenize the data, 
-fTest      = open(testCSV,'rb')
-validation = csv.reader(fTest)
+#cast to vector form
+trainingY = np.array(trainingY)
 
-counter = 0
-for row in validation:
-    if (row[0]).isdigit():
-        text       = nltk.word_tokenize(row[1].decode('utf-8'))
-        index      = int(row[0])
-        if index % 1000 ==0:
-            print index ,' of validation'
-        validationSet[index] = text
+#normalized count vector, could use CountVectorizer() instead
+#word needs to appear twice and not be a stop word.
+normalizedCountVectorizer = TfidfVectorizer(min_df=2,stop_words='english')
 
-   
+X = normalizedCountVectorizer.fit_transform(trainingCorpus)
 
-#dump the dictionarys using pickle so we dont have to reparse every time
-with open("./DataSetDictionarys/trainingSet.txt","wb") as trainFile:
-    pickle.dump(trainingSet,trainFile)
+
+
+fTest            = open(testCSV,'rb')
+validation       = csv.reader(fTest)
+
+testingExamples  = [row[1] for row in validation if row[0].isdigit()]
+
+Xtest            = normalizedCountVectorizer.transform(testingExamples)
+
+print "X shape = ", X.shape, "Y shape = ", trainingY.shape , "testX shape = ", Xtest.shape
+
+
+#pickle it to be opened later
+with open("./DataSetDictionarys/trainingSetX.txt","wb") as trainFileX:
+    pickle.dump(X,trainFileX)
 
 with open("./DataSetDictionarys/validationSet.txt","wb") as validationFile:
-    pickle.dump(validationSet,validationFile)
+    pickle.dump(Xtest,validationFile)
+
+with open("./DataSetDictionarys/trainingSetY.txt","wb") as trainFileY:
+    pickle.dump(trainingY,trainFileY)
+
+print normalizedCountVectorizer.get_feature_names()
+
+
+
+
     
-print len(trainingSet), ',' ,len(validationSet)
-    
+
+
 
