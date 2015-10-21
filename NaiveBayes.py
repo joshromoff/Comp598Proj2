8cc3bc415b1ef8da315db7808563c0c0d5fa084d
Eric Quinn <EJBQuinn@gmail.com>
2015-10-21 00:43:28 -0400

    Adding correct Naive Bayes

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -8,7 +8,10 @@ Created on Sun Oct 11 19:45:18 2015
 import numpy as np
 import pickle
 import math
-from sklearn import cross_validation
+import sklearn
+import scipy as sp
+import csv
+import string
 
 parameters = None
 numOutputs = {}
@@ -154,7 +157,7 @@ def predict(data, distrs, usetfidf = False):
     for i in range(data.shape[0]) :
         #for each input
         #print i, data.shape[0], np.shape(distrs)
-        maxProb = 1e-300
+        maxProb = -1e300
         toRetOutput = -1
         for output in range(len(distrs[1])) :
             p = probabOfOutput[output]
@@ -168,6 +171,7 @@ def predict(data, distrs, usetfidf = False):
                     probSum += binaryPredictAdd(output, data, i, j)
             totalProb = bias + probSum
             if totalProb > maxProb :
+                maxProb = totalProb
                 toRetOutput = output
         Y.extend([toRetOutput])
         
@@ -176,7 +180,7 @@ def predict(data, distrs, usetfidf = False):
 def tfidfPredictAdd(output, data, i, j) :
     prob = normpdf(data[i,output], j[output][0], j[output][1])
     probGivenNot = normpdf(data[i,output], j[output][2], j[output][3])         
-    return np.log(prob/probGivenNot)
+    return np.log(prob) - np.log(probGivenNot)
 
 def binaryPredictAdd(output, data, i, j) :
     probGivenCat = j[output][0]
@@ -189,7 +193,7 @@ def binaryPredictAdd(output, data, i, j) :
         denominator = 1 - denominator
     if denominator == 0 :
         return 0
-    return np.log(numerator * 1.0 /denominator)
+    return np.log(numerator)  - np.log(denominator)
 
 #source : http://stackoverflow.com/questions/12412895/calculate-probability-in-normal-distribution-given-mean-std-in-python
 def normpdf(x, mean, sd):
@@ -220,6 +224,114 @@ def getSuccessRate(guess, true) :
 
     return correct * 1.0 / total    
 
+def getCorpusData() :
+    
+    testCSV  = "./DataSetCSVs/ml_dataset_test_in.csv"
+    trainCSV = "./DataSetCSVs/ml_dataset_train.csv"
+    
+    f = open(trainCSV,'rb')
+    training = csv.reader(f)
+
+    trainingCorpus = []
+    trainingY      = []
+
+    #loop through training set, accumulated the text 
+    for row in training:
+        if row[0].isdigit():
+            trainingCorpus.append(row[1])
+            trainingY.append(int(row[2]))
+            
+    skf = sklearn.cross_validation.StratifiedKFold(trainingY, n_folds=4)
+
+    trainingCorpus = np.array(trainingCorpus)
+    trainingY = np.array(trainingY)
+
+    for train_index, test_index in skf :
+        X_train, X_test = trainingCorpus[train_index], trainingCorpus[test_index]
+        y_train, y_test = trainingY[train_index], trainingY[test_index]
+
+    probabilities, categoryRatio, vocabSize, corpusSizeInCat = trainLaplace(X_train, y_train)
+    
+    allString = '0123'
+    correct = 0
+    total = 0
+    for row in range(len(X_test)):
+        line = "".join([ch for ch in X_test[row] if ch not in string.punctuation])
+        toRetOutput = -1
+        maxProb = -1e300
+        for i in range(4) :
+            p = categoryRatio[i]
+            bias = np.log(p/(1-p))
+            probSum = 0
+            for word in line.split(" ") :
+                # for each feature
+                if (word, i) in probabilities :
+                    prob = np.log(probabilities(word, i)) -  np.log(probabilities(word, allString.replace(str(i), '')))                        
+                else :
+                    prob = prob = (np.log(1 ) - np.log(corpusSizeInCat[i] + vocabSize)) - (np.log(1) - np.log(sum(corpusSizeInCat) - corpusSizeInCat[i] + vocabSize))
+                probSum += prob
+            totalProb = bias + probSum
+            if totalProb > maxProb :
+                maxProb = totalProb
+                toRetOutput = i
+        guess = toRetOutput
+        if guess == y_test[row] :
+            correct += 1
+        total += 1
+    print correct * 1. / total    
+            
+def trainLaplace(X_train, y_train) :
+    
+    trainCorpus = ["","","",""]
+    for i in range(len(X_train)) :
+        trainCorpus[y_train[i]] += " " + X_train[i]
+
+    for i in range(len(trainCorpus)) :
+        trainCorpus[i] = "".join([ch for ch in trainCorpus[i] if ch not in string.punctuation])
+
+    countVectorizer = sklearn.feature_extraction.text.CountVectorizer()
+    X = countVectorizer.fit_transform(trainCorpus)
+
+    numIn  = []
+    for num in range(4) :
+        numIn.extend(np.array(sp.sparse.csr_matrix.sum(X, 1)[num])[0])
+
+    vocabSize = X.shape[1]
+    
+    probabilities = {}
+    featureNames = countVectorizer.get_feature_names()    
+    print featureNames
+    
+    allString = "0123"
+    corpusSizeWithoutCategory = []
+    for i in range(4) :
+        corpusSizeWithoutCategory.extend([sum(numIn) - numIn[i] + vocabSize])
+        
+    for i in range(4) :
+        #we find the probabilities for each word
+        for word in range(X.shape[1]) : #for each word/feature
+            if word % 1000 == 0 :
+                print word, i
+            #use laplace smoothing and estimate values
+            #print X.shape, np.shape(numIn), type(numIn[i]), numIn[i]+vocabSize, word, i
+            probabilities[(featureNames[word], str(i))] = (X[i, word] + 1.) / (numIn[i] + vocabSize)
+            probabilities[(featureNames[word], allString.replace(str(i), ''))] = (np.array(sp.sparse.csr_matrix.sum(X, 0))[0][word] - X[i, word] + 1.) / corpusSizeWithoutCategory[i]
+    
+    categoryRatio = {}
+    totalOutputs = {}
+    total = 0
+    for i in range(trainingY.shape[0]) :
+        output = trainingY[i]
+        if output not in totalOutputs :
+            totalOutputs[output] = 0
+        totalOutputs[output] += 1 
+        total += 1
+    
+    for key in totalOutputs.keys() :
+        categoryRatio[key] = totalOutputs[key] * 1.0 / total    
+    
+    return probabilities, categoryRatio, vocabSize, numIn
+
 X=None
 Xtest=None
 trainingY=None
@@ -234,30 +346,33 @@ with open("./DataSetDictionarys/validationSet2.txt","rb") as validationFile:
 with open("./DataSetDictionarys/trainingSetY2.txt","rb") as trainFileY:
     trainingY = pickle.load(trainFileY)
 
-from sklearn.feature_selection import VarianceThreshold
-from sklearn.feature_selection import f_classif
-from sklearn.feature_selection import SelectKBest
 
-#K best features
-KBESTNUM = 200
- 
-print "ORIGINAL #FEATURES", X.shape[1]
-print "removing features with zero variance"
-sel = VarianceThreshold(threshold = (.000005))
-X = sel.fit_transform(X)
-print "feature selecting using " +str(KBESTNUM)
+getCorpusData()
 
-kBest         = SelectKBest(f_classif,k=KBESTNUM)
-X     = kBest.fit_transform(X,trainingY)
+#from sklearn.feature_selection import VarianceThreshold
+#from sklearn.feature_selection import f_classif
+#from sklearn.feature_selection import SelectKBest
 
-print "done selecting features"
-
-skf = cross_validation.StratifiedKFold(trainingY, n_folds=4)
-
-for train_index, test_index in skf:
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = trainingY[train_index], trainingY[test_index]
-
-    features = fitBinaryNaiveBayes(X_train, y_train)
-    Yprime = predict(X_test, features)
-    print(getSuccessRate(Yprime, y_test))
+##K best features
+#KBESTNUM = 200
+#
+#print "ORIGINAL #FEATURES", X.shape[1]
+#print "removing features with zero variance"
+#sel = VarianceThreshold(threshold = (.000005))
+#X = sel.fit_transform(X)
+#print "feature selecting using " +str(KBESTNUM)
+#
+#kBest         = SelectKBest(f_classif,k=KBESTNUM)
+#X     = kBest.fit_transform(X,trainingY)
+#
+#print "done selecting features"
+#
+#skf = sklearn.cross_validation.StratifiedKFold(trainingY, n_folds=4)
+#
+#for train_index, test_index in skf:
+#    X_train, X_test = X[train_index], X[test_index]
+#    y_train, y_test = trainingY[train_index], trainingY[test_index]
+#
+#    features = fitBinaryNaiveBayes(X_train, y_train)
+#    Yprime = predict(X_test, features)
+#    print(getSuccessRate(Yprime, y_test))
