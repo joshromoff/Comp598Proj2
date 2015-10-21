ca5b870d9dd31a105fe3e0b6f1ab64f475d00916
Eric Quinn <EJBQuinn@gmail.com>
2015-10-21 03:08:25 -0400

    Adding smoothing factor and separating NB into methods

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -12,10 +12,53 @@ import sklearn
 import scipy as sp
 import csv
 import string
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.feature_selection import f_classif
+from sklearn.feature_selection import SelectKBest
 
 parameters = None
 numOutputs = {}
 probabOfOutput = {}  
+
+def doLimitedSetNaiveBayes() :
+
+    X=None
+    Xtest=None
+    trainingY=None
+        
+    #we use the count vectorizer output, not the tf-idf output
+    with open("./DataSetDictionarys/trainingSetX2.txt","rb") as trainFileX:
+        X = pickle.load(trainFileX)
+    
+    with open("./DataSetDictionarys/validationSet2.txt","rb") as validationFile:
+        Xtest = pickle.load(validationFile)
+    
+    with open("./DataSetDictionarys/trainingSetY2.txt","rb") as trainFileY:
+        trainingY = pickle.load(trainFileY)
+
+    #K best features
+    KBESTNUM = 200
+    
+    print "ORIGINAL #FEATURES", X.shape[1]
+    print "removing features with zero variance"
+    sel = VarianceThreshold(threshold = (.000005))
+    X = sel.fit_transform(X)
+    print "feature selecting using " +str(KBESTNUM)
+    
+    kBest         = SelectKBest(f_classif,k=KBESTNUM)
+    X     = kBest.fit_transform(X,trainingY)
+    
+    print "done selecting features"
+    
+    skf = sklearn.cross_validation.StratifiedKFold(trainingY, n_folds=4)
+    
+    for train_index, test_index in skf:
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = trainingY[train_index], trainingY[test_index]
+    
+        features = fitBinaryNaiveBayes(X_train, y_train)
+        Yprime = predict(X_test, features, usetfidf = False)
+        print(getSuccessRate(Yprime, y_test))
 
 def fitTFIDFNaiveBayes(data, trainingY):
     #data is expected to be a matrix with the final columns as the outputs
@@ -77,11 +120,11 @@ def fitTFIDFNaiveBayes(data, trainingY):
             std = np.std(np.append(np.array(outputVals), np.zeros((numOutputsOfCateg - numNonZeros , 1))))
             
             #distribution of data in other categories
-            others = []
+            othersNonZeros = []
             for category in feature :
-                others.extend([datum for datum in category if (category != outputVals)])
-            meanNotOutput = np.mean(np.append(np.array(others), np.zeros((numOtherCats - len(others) , 1))))
-            stdNotOutput = np.std(np.append(np.array(others), np.zeros((numOtherCats - len(others) , 1))))
+                othersNonZeros.extend([datum for datum in category if (feature.index(category) != i)])
+            meanNotOutput = np.mean(np.append(np.array(othersNonZeros), np.zeros((numOtherCats - len(othersNonZeros) , 1))))
+            stdNotOutput = np.std(np.append(np.array(othersNonZeros), np.zeros((numOtherCats - len(othersNonZeros) , 1))))
             
             byFeature.append([mean, std, meanNotOutput, stdNotOutput])
   
@@ -224,7 +267,11 @@ def getSuccessRate(guess, true) :
 
     return correct * 1.0 / total    
 
-def getCorpusData() :
+
+
+
+
+def doFullNaiveBayes() :
     
     testCSV  = "./DataSetCSVs/ml_dataset_test_in.csv"
     trainCSV = "./DataSetCSVs/ml_dataset_train.csv"
@@ -250,37 +297,13 @@ def getCorpusData() :
         X_train, X_test = trainingCorpus[train_index], trainingCorpus[test_index]
         y_train, y_test = trainingY[train_index], trainingY[test_index]
 
-    probabilities, categoryRatio, vocabSize, corpusSizeInCat = trainLaplace(X_train, y_train)
-    
-    allString = '0123'
-    correct = 0
-    total = 0
-    for row in range(len(X_test)):
-        line = "".join([ch for ch in X_test[row] if ch not in string.punctuation])
-        toRetOutput = -1
-        maxProb = -1e300
-        for i in range(4) :
-            p = categoryRatio[i]
-            bias = np.log(p/(1-p))
-            probSum = 0
-            for word in line.split(" ") :
-                # for each feature
-                if (word, i) in probabilities :
-                    prob = np.log(probabilities(word, i)) -  np.log(probabilities(word, allString.replace(str(i), '')))                        
-                else :
-                    prob = prob = (np.log(1 ) - np.log(corpusSizeInCat[i] + vocabSize)) - (np.log(1) - np.log(sum(corpusSizeInCat) - corpusSizeInCat[i] + vocabSize))
-                probSum += prob
-            totalProb = bias + probSum
-            if totalProb > maxProb :
-                maxProb = totalProb
-                toRetOutput = i
-        guess = toRetOutput
-        if guess == y_test[row] :
-            correct += 1
-        total += 1
-    print correct * 1. / total    
+    smooth_factor = .01
+
+    probabilities, categoryRatio, vocabSize, corpusSizeInCat = trainLaplace(X_train, y_train, smooth_factor)
+    YPrime = makeFullPredictions(probabilities, vocabSize, corpusSizeInCat, categoryRatio, smooth_factor, X_test, y_test)
+    getSuccessRate(YPrime, y_test)
             
-def trainLaplace(X_train, y_train) :
+def trainLaplace(X_train, y_train, smooth_factor) :
     
     trainCorpus = ["","","",""]
     for i in range(len(X_train)) :
@@ -300,12 +323,11 @@ def trainLaplace(X_train, y_train) :
     
     probabilities = {}
     featureNames = countVectorizer.get_feature_names()    
-    print featureNames
     
     allString = "0123"
     corpusSizeWithoutCategory = []
     for i in range(4) :
-        corpusSizeWithoutCategory.extend([sum(numIn) - numIn[i] + vocabSize])
+        corpusSizeWithoutCategory.extend([sum(numIn) - numIn[i] + vocabSize * smooth_factor])
         
     for i in range(4) :
         #we find the probabilities for each word
@@ -314,8 +336,8 @@ def trainLaplace(X_train, y_train) :
                 print word, i
             #use laplace smoothing and estimate values
             #print X.shape, np.shape(numIn), type(numIn[i]), numIn[i]+vocabSize, word, i
-            probabilities[(featureNames[word], str(i))] = (X[i, word] + 1.) / (numIn[i] + vocabSize)
-            probabilities[(featureNames[word], allString.replace(str(i), ''))] = (np.array(sp.sparse.csr_matrix.sum(X, 0))[0][word] - X[i, word] + 1.) / corpusSizeWithoutCategory[i]
+            probabilities[(featureNames[word], str(i))] = (X[i, word] + smooth_factor) / (numIn[i] + vocabSize * smooth_factor)
+            probabilities[(featureNames[word], allString.replace(str(i), ''))] = (np.array(sp.sparse.csr_matrix.sum(X, 0))[0][word] - X[i, word] + smooth_factor) / corpusSizeWithoutCategory[i]
     
     categoryRatio = {}
     totalOutputs = {}
@@ -332,47 +354,34 @@ def trainLaplace(X_train, y_train) :
     
     return probabilities, categoryRatio, vocabSize, numIn
 
-X=None
-Xtest=None
-trainingY=None
-    
-#we use the count vectorizer output, not the tf-idf output
-with open("./DataSetDictionarys/trainingSetX2.txt","rb") as trainFileX:
-    X = pickle.load(trainFileX)
+def makeFullPredictions(probabilities, vocabSize, corpusSizeInCat, categoryRatio, smooth_factor, X_test, y_test) :
+    allString = '0123'
+    Y = []
+    for row in range(len(X_test)):
+        line = "".join([ch for ch in X_test[row] if ch not in string.punctuation])
+        toRetOutput = -1
+        maxProb = -1e300
+        for i in range(4) :
+            p = categoryRatio[i]
+            bias = np.log(p/(1-p))
+            probSum = 0
+            for word in line.split(" ") :
+                # for each feature
+                if (word, i) in probabilities :
+                    prob = np.log(probabilities(word, i)) -  np.log(probabilities(word, allString.replace(str(i), '')))                        
+                else :
+                    prob = prob = (np.log(smooth_factor) - np.log(corpusSizeInCat[i] + vocabSize * smooth_factor)) - (np.log(smooth_factor) - np.log(sum(corpusSizeInCat) - corpusSizeInCat[i] + vocabSize * smooth_factor))
+                if math.isinf(prob) or math.isnan(prob) :
+                    prob = 0
+                probSum += prob
+            totalProb = bias + probSum
+            if totalProb > maxProb :
+                maxProb = totalProb
+                toRetOutput = i
+        Y.extend([toRetOutput])
+        
+    return Y
 
-with open("./DataSetDictionarys/validationSet2.txt","rb") as validationFile:
-    Xtest = pickle.load(validationFile)
 
-with open("./DataSetDictionarys/trainingSetY2.txt","rb") as trainFileY:
-    trainingY = pickle.load(trainFileY)
-
-
-getCorpusData()
-
-#from sklearn.feature_selection import VarianceThreshold
-#from sklearn.feature_selection import f_classif
-#from sklearn.feature_selection import SelectKBest
-
-##K best features
-#KBESTNUM = 200
-#
-#print "ORIGINAL #FEATURES", X.shape[1]
-#print "removing features with zero variance"
-#sel = VarianceThreshold(threshold = (.000005))
-#X = sel.fit_transform(X)
-#print "feature selecting using " +str(KBESTNUM)
-#
-#kBest         = SelectKBest(f_classif,k=KBESTNUM)
-#X     = kBest.fit_transform(X,trainingY)
-#
-#print "done selecting features"
-#
-#skf = sklearn.cross_validation.StratifiedKFold(trainingY, n_folds=4)
-#
-#for train_index, test_index in skf:
-#    X_train, X_test = X[train_index], X[test_index]
-#    y_train, y_test = trainingY[train_index], trainingY[test_index]
-#
-#    features = fitBinaryNaiveBayes(X_train, y_train)
-#    Yprime = predict(X_test, features)
-#    print(getSuccessRate(Yprime, y_test))
+doFullNaiveBayes()
+#doLimitedSetNaiveBayes()
